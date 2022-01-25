922a822644ef6da25fe5ae2112c261151dc8005e
Tammarra McPhaul <shaemc22@gmail.com>
2022-01-24 20:25:25 -0500

    commit tree db90127ef5baa1c3e8cbc4b1bb84a1f5955ad34e

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, str) {
+    return drivers.filter(d => 
+         d.toLowerCase() === str.toLowerCase()
+    )
+}
+
+function fuzzyMatch(drivers,str) {
+    return drivers.filter(d => d.toLowerCase().indexOf(str.toLowerCase()) === 0)
+}
+
+function matchName(drivers, str) {
+    return drivers.filter( d => d.name === str)
+}
